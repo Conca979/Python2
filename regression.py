3e9fbc55df69055ca1d12ee920ddbfa12ec2a034
Duong <tn7956666@gail.com>
2026-03-19 14:52:01 +0700

    hub?

--- regression.py
+++ regression.py
@@ -2,56 +2,115 @@ import matplotlib.pyplot as plt
 import math
 
 class basicLinearRegression: # gradinet decsent approach
-  def __init__(self, inputTrainingSet, inputParameters = None, epsilon = 0.001, learningRate = 0.001, DataSplittingMethod = None):
+  def __init__(self, 
+               inputDataSet, 
+               initWeights = None, 
+               epsilon = 0.000001, 
+               learningRate = 0.001, 
+               parsingData = (None,None,None),  # [0]: comma/pip/semicolumn/space-separated value
+                                                # [1]: header included in file
+                                                # [2]: indexing column included
+               featureScaling = True,
+               outComeScaling = True):
     # get dataSet
-    if isinstance(inputTrainingSet, str):
-      with open(inputTrainingSet) as f:
-        next(f)
-        self.trainingSet = [[float(val) for val in (line.split()[1:] if DataSplittingMethod is None else line.split(DataSplittingMethod)[1:])] for line in f]
+    if isinstance(inputDataSet, str):
+      with open(inputDataSet) as f:
+        if parsingData[1]: next(f) # skip the header line
+        offSet = 1 if parsingData[2] else 0 # skip the index column
+        self.dataSet = [[float(val) for val in line.split(parsingData[0])[offSet:]] for line in f]
     else:
-      self.trainingSet = inputTrainingSet
-    #
-    if inputParameters is None:
-      self.parameters = [0]*len(self.trainingSet[0])
-    else:
-      self.parameters = inputParameters
-    #
-    self.sizeDataSet = len(self.trainingSet)
+      self.dataSet = inputDataSet
+    # model initialization
+    self.sizeDataSet = len(self.dataSet)
+    self.featureCount = len(self.dataSet[0]) - 1
     self.learningRate = learningRate
     self.epsilon = epsilon
     self.iterationCount = 0
-    self._featureScaling()
+    # initialize weights
+    if initWeights is None:
+      self.weights = [0]*len(self.dataSet[0])
+    else:
+      self.weights = initWeights
+    # Scaling data to avoid overFlow
+    self.outComeScaling = False
+    self.featureScaling = False
+    if featureScaling:
+      self.featureScaling = self._featureScaling()
+    if outComeScaling:
+      self.outComeScaling = self._outComeScaling()
+
+  def _outComeScaling(self): # Z-score standardization
+    # new scaled feature = (oldFeature - theMean) / theStandardDeviation
+    mean = sum([self.dataSet[i][-1] / self.sizeDataSet for i in range(self.sizeDataSet)])
+    stdDev = (sum([((self.dataSet[i][-1] - mean)**2) / self.sizeDataSet for i in range(self.sizeDataSet)]))**0.5
+    for i in range(self.sizeDataSet):
+      if stdDev > 0:
+        self.dataSet[i][-1] = (self.dataSet[i][-1] - mean) / stdDev
+      else:
+        self.dataSet[i][-1] = 0
+
+    return mean, stdDev
 
   def _featureScaling(self): # Z-score standardization
-    # new scaled feature = (feature - theMean) / theStandardDeviation
-    for featureIndex in range(len(self.trainingSet[0]) - 1):
-      mean = sum([self.trainingSet[i][featureIndex] for i in range(self.sizeDataSet)]) / self.sizeDataSet
-      stdDeviation = (sum([(self.trainingSet[i][featureIndex] - mean)**2 for i in range(self.sizeDataSet)]) / self.sizeDataSet)**0.5
-      for i in range(len(self.trainingSet)):
-        if stdDeviation > 0:
-          self.trainingSet[i][featureIndex] = (self.trainingSet[i][featureIndex] - mean) / stdDeviation
+    # new scaled feature = (oldFeature - theMean) / theStandardDeviation
+    scalingParams = [] # store the (mean, stdDev)
+    for featureIndex in range(self.featureCount):
+      mean = sum([self.dataSet[i][featureIndex] / self.sizeDataSet for i in range(self.sizeDataSet)])
+      stdDev = (sum([((self.dataSet[i][featureIndex] - mean)**2) / self.sizeDataSet for i in range(self.sizeDataSet)]))**0.5
+      scalingParams.append((mean, stdDev))
+      for i in range(self.sizeDataSet):
+        if stdDev > 0:
+          self.dataSet[i][featureIndex] = (self.dataSet[i][featureIndex] - mean) / stdDev
         else:
-          self.trainingSet[i][featureIndex] = 0     
+          self.dataSet[i][featureIndex] = 0
 
-  def predict(self, sampleIndex): # from the hypothesis function
-    result = self.parameters[0]
-    for i in range(len(self.trainingSet[0]) - 1):
-      result += self.parameters[i + 1]*self.trainingSet[sampleIndex][i]
+    return scalingParams
+  
+  def _modelEvalutaion(self): # R-square evaluation
+    actualMean = sum([val[-1] for val in self.dataSet]) / self.sizeDataSet
+    sst = 0 # sumOfSquareErrors: proportional to the variance of the actual data
+    sse = 0 # sumOfSquareOfResidualErrors: proportional to the variance of the prodicted data
+    for sampleIndex in range(self.sizeDataSet):
+      sst += (self.dataSet[sampleIndex][-1] - actualMean)**2
+      sse += (self.dataSet[sampleIndex][-1] - self._predict(sampleIndex))**2
+    
+    goodnessOfFit = (1 - (sse / sst))
+    return sst, sse, goodnessOfFit
+  
+  def predict(self, inputData):
+    if len(inputData) == self.featureCount:
+      result = self.weights[0]
+      for i in range(self.featureCount):
+        if self.featureScaling:
+          mean, stdDev = self.featureScaling[i][0], self.featureScaling[i][1]
+          inputData[i] = (inputData[i] - mean) / stdDev
+        result += self.weights[i + 1]*inputData[i]
+
+      if self.outComeScaling:
+        result = result*self.outComeScaling[1] + self.outComeScaling[0]
+      return result
+    else:
+      print("Input size does not match the model! pls check again")
+      return None
+
+  def _predict(self, sampleIndex): # from the hypothesis function
+    result = self.weights[0]
+    for i in range(self.featureCount):
+      result += self.weights[i + 1]*self.dataSet[sampleIndex][i]
 
     return result
   
   def computeMse(self): # the mean squared error
     e = 0
-    m = len(self.trainingSet)
-    for sampleIndex in range(m):
-      e += ((self.predict(sampleIndex)) - self.trainingSet[sampleIndex][-1])**2
+    for sampleIndex in range(self.sizeDataSet):
+      e += ((self._predict(sampleIndex)) - self.dataSet[sampleIndex][-1])**2
 
-    return e / (m*2)
+    return e / (self.sizeDataSet*2)
   
-  def computeGradient(self, featureIndex): # from cost function
+  def computeGradient(self, featureIndex):
     result = 0
     for sampleIndex in range(self.sizeDataSet):
-      result += (self.trainingSet[sampleIndex][-1] - self.predict(sampleIndex))*(1 if featureIndex == 0 else self.trainingSet[sampleIndex][featureIndex - 1])
+      result += (self.dataSet[sampleIndex][-1] - self._predict(sampleIndex))*(1 if featureIndex == 0 else self.dataSet[sampleIndex][featureIndex - 1])
 
     return result*(self.learningRate / self.sizeDataSet)
 
@@ -62,18 +121,19 @@ class basicLinearRegression: # gradinet decsent approach
       with open('iterations.txt', mode = 'w') as f: pass
       with open('iterations.txt', mode = 'a') as f:
         f.write(f"[weights], current MSE, shifting in MSE \n")
+        f.write(f"{self.weights}, {currentCost}, 0 \n")
         while True:
           self.iterationCount += 1
-          updatePar = self.parameters.copy()
-          for featureIndex in range(len(self.parameters)):
-            updatePar[featureIndex] += self.computeGradient(featureIndex)
+          updWeights = self.weights.copy()
+          for weightIndex in range(self.featureCount + 1):
+            updWeights[weightIndex] += self.computeGradient(weightIndex)
 
-          self.parameters = updatePar
+          self.weights = updWeights
           updateCost = self.computeMse()
           self.costHistory.append(updateCost)
           shifting = abs((currentCost - updateCost) / currentCost)
           #
-          f.write(f"{self.parameters}, {updateCost}, {shifting} \n")
+          f.write(f"{self.weights}, {updateCost}, {shifting} \n")
           #
           if (shifting) < self.epsilon or self.iterationCount > 200000:
             break
@@ -82,11 +142,11 @@ class basicLinearRegression: # gradinet decsent approach
     else:
       while True:
         self.iterationCount += 1
-        updatePar = self.parameters.copy()
-        for featureIndex in range(len(self.parameters)):
-          updatePar[featureIndex] += self.computeGradient(featureIndex)
+        updatePar = self.weights.copy()
+        for weightIndex in range(self.featureCount + 1):
+          updatePar[weightIndex] += self.computeGradient(weightIndex)
 
-        self.parameters = updatePar
+        self.weights = updatePar
         updateCost = self.computeMse()
         self.costHistory.append(updateCost)
         shifting = abs((currentCost - updateCost) / currentCost)
@@ -97,11 +157,10 @@ class basicLinearRegression: # gradinet decsent approach
         else:
           currentCost = updateCost
 
-
-  def showModel(self, graph = None): # type = Prediction Accurary by default, Cost vs Iteration for any passed value
+  def showModel(self, graph = None): # graphType = "Prediction Accurary" by default, "Cost vs Iteration" for any passed value
     if graph is None:
-      predictedValue = [self.predict(sampleIndex) for sampleIndex in range(len(self.trainingSet))]
-      actualValue = [ys[-1] for ys in self.trainingSet]
+      predictedValue = [self._predict(sampleIndex) for sampleIndex in range(self.sizeDataSet)]
+      actualValue = [ys[-1] for ys in self.dataSet]
 
       plt.scatter(actualValue, predictedValue, c = 'r')
       plt.xlabel('Predicted Values')
@@ -111,18 +170,18 @@ class basicLinearRegression: # gradinet decsent approach
       lowerBound = min((min(predictedValue), min(actualValue)))
       plt.plot([lowerBound, upperBound], [lowerBound, upperBound], c = 'b', alpha = 0.5)
 
-      # t1 = [[actualValue[k], predictedValue[k]] for k in range(len(self.trainingSet))]
+      # t1 = [[actualValue[k], predictedValue[k]] for k in range(self.sizeDataSet)]
       # model = basicLinearRegression(t1, epsilon= self.epsilon, learningRate= self.learningRate)
       # model.fitModel()
-      # t = model.parameters
+      # t = model.weights
       # plt.plot([lowerBound, upperBound], [lowerBound*t[1] + t[0], upperBound*t[1] + t[0]], c = 'r')
       #
       plt.title('Prediction Accurary')
     else:
       plt.xlabel('Iteration (Log scale)')
       plt.ylabel('Cost - MSE')
-      plt.xscale('log')
       plt.title('Cost vs iteration')
+      if self.iterationCount >= 1000: plt.xscale('log')
       plt.plot(self.costHistory)
 
     plt.show()
@@ -144,7 +203,7 @@ class basicLogisticRegression: # grandient ascent approach
       self.weights = weights
     #
 
-  def predict(self, sampleIndex):
+  def _predict(self, sampleIndex):
     result = self.weights[0]
     for i in range(len(self.weights) - 1):
       result += self.weights[i + 1]*self.dataSet[sampleIndex][i]
@@ -154,7 +213,7 @@ class basicLogisticRegression: # grandient ascent approach
   def computeLogLikelihood(self):
     llh = 0 # log Likelihood
     for sampleIndex in range(self.sizeDataSet):
-      llh += self.dataSet[sampleIndex][-1]*math.log(self.predict(sampleIndex)) + (1 - self.dataSet[sampleIndex][-1])*math.log(1 - self.predict(sampleIndex))
+      llh += self.dataSet[sampleIndex][-1]*math.log(self._predict(sampleIndex)) + (1 - self.dataSet[sampleIndex][-1])*math.log(1 - self._predict(sampleIndex))
 
     return llh
   
@@ -162,7 +221,7 @@ class basicLogisticRegression: # grandient ascent approach
     grd = 0
     
     for sampleIndex in range(self.sizeDataSet):
-      grd += (self.dataSet[sampleIndex][-1] - self.predict(sampleIndex))*(1 if freatureIndex == 0 else self.dataSet[sampleIndex][freatureIndex - 1])
+      grd += (self.dataSet[sampleIndex][-1] - self._predict(sampleIndex))*(1 if freatureIndex == 0 else self.dataSet[sampleIndex][freatureIndex - 1])
 
     return self.learningRate*grd
   
@@ -195,7 +254,7 @@ class basicLogisticRegression: # grandient ascent approach
       testSet = [[float(val) for val in line.split()] for line in f]
 
     self.dataSet = testSet
-    predictedProbabilities = [self.predict(sampleIndex) for sampleIndex in range(len(testSet))]
+    predictedProbabilities = [self._predict(sampleIndex) for sampleIndex in range(len(testSet))]
     predictedLabels = [(1 if val >= 0.5 else 0) for val in predictedProbabilities]
     tableData = []
     for i in range(len(testSet)):
@@ -228,10 +287,14 @@ class basicLogisticRegression: # grandient ascent approach
     # print([True if (1 if predictedProbability[i] >= 0.5 else 0) == testSet[i][-1] else False for i in range(len(testSet))])
 
 
-model = basicLinearRegression('Advertising.csv', epsilon = 0.00000001, learningRate = 0.0001, DataSplittingMethod= ',')
-model.fitModel(log= False)
+model = basicLinearRegression('Advertising.csv', 
+                              epsilon= 0.00000001, 
+                              learningRate= 0.0001, 
+                              parsingData= (',', True, True))
+model.fitModel(log= True)
 print(model.iterationCount)
-model.showModel(graph= 'C')
+model.showModel()
+print(model._modelEvalutaion())
 
 # model = basicLogisticRegression('data2.txt', epsilon= 0.00001, learningRate= 0.001)
 # model.fitModel()
