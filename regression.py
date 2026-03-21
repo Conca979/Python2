d6c9b3ddc9e269be1983fcdefdd376f4a44d1ed6
Duong <tn7956666@gail.com>
2026-03-21 14:45:54 +0700

    model polynomial transformation

--- regression.py
+++ regression.py
@@ -3,40 +3,49 @@ import math
 import random
 
 class modelSetup:
-  def __init__(self, inputDataSet, initWeights, epsilon, learningRate, parsingData, featureScaling):
+  def __init__(self, inputDataSet, initWeights, epsilon, learningRate, parsingData, featureScaling, modelDegree, iterationLogTrigger):
     # get dataSet
     self.parsingData = parsingData
     self._dataPreparation(inputDataSet)
-    # model initialization
     self.sizeDataSet = len(self.dataSet)
-    self.featureCount = len(self.dataSet[0]) - 1
+    # Scaling data to avoid overFlow
+    self.scalers = []
+    self.featureScaling = featureScaling
+    self._featureScaling()
+    # polynomial model transformation
+    self.degree = modelDegree
+    self.polyTransform()
+    self._featureScaling(skipBias= True)
+    # model initialization
+    self.featureCount = len(self.dataSet[0]) - 1 # bias part included, excluding label part
     self.learningRate = learningRate
     self.epsilon = epsilon
     self.iterationCount = 0
+    self.logTrigger = iterationLogTrigger
     # initialize weights
     if initWeights is None:
-      self.weights = [0]*len(self.dataSet[0])
+      self.weights = [0]*self.featureCount
     else:
       self.weights = initWeights
-    # Scaling data to avoid overFlow
-    self.featureScaling = False
-    if featureScaling:
-      self.featureScaling = self._featureScaling()
 
-  def _featureScaling(self): # Z-score standardization
+  def _featureScaling(self, skipBias= False): # Z-score standardization
     # new scaled feature = (oldFeature - theMean) / theStandardDeviation
-    scalingParams = [] # store the (mean, stdDev)
-    for featureIndex in range(self.featureCount):
-      mean = sum([self.dataSet[i][featureIndex] / self.sizeDataSet for i in range(self.sizeDataSet)])
-      stdDev = (sum([((self.dataSet[i][featureIndex] - mean)**2) / self.sizeDataSet for i in range(self.sizeDataSet)]))**0.5
-      scalingParams.append((mean, stdDev))
-      for i in range(self.sizeDataSet):
-        if stdDev > 0:
-          self.dataSet[i][featureIndex] = (self.dataSet[i][featureIndex] - mean) / stdDev
-        else:
-          self.dataSet[i][featureIndex] = 0
+    if self.featureScaling:
+      scalingParams = [] # store the (mean, stdDev)
+      featureCount = len(self.dataSet[0]) - 1
+      for featureIndex in range((1 if skipBias else 0), featureCount):
+        mean = sum([self.dataSet[i][featureIndex] / self.sizeDataSet for i in range(self.sizeDataSet)])
+        stdDev = (sum([((self.dataSet[i][featureIndex] - mean)**2) / self.sizeDataSet for i in range(self.sizeDataSet)]))**0.5
+        scalingParams.append((mean, stdDev))
+        for i in range(self.sizeDataSet):
+          if stdDev > 0:
+            self.dataSet[i][featureIndex] = (self.dataSet[i][featureIndex] - mean) / stdDev
+          else:
+            self.dataSet[i][featureIndex] = 0
 
-    return scalingParams
+      self.scalers.append(scalingParams)
+    else:
+      return
 
   def _dataPreparation(self, inputDataSet):
     # preparing the dataSet
@@ -53,18 +62,57 @@ class modelSetup:
       self.dataSet = inputDataSet[:int(len(inputDataSet)*0.8)]
       self.testSet = inputDataSet[int(len(inputDataSet)*0.8):]
 
-class basicLinearRegression(modelSetup): # gradinet decsent approach
+  class polyFeatureTransform:
+    def __init__(self, featureCount, degree= 1):
+      self.relations = []
+      self.degree = degree
+      self.featureCount = featureCount
+      # initializing relations
+      self._getFeatureRelation()
+
+    def _gen(self, cur, start, degNeeded):
+      if degNeeded == 0:
+        self.relations.append(cur.copy())
+        return
+      for e in range(start, self.featureCount):
+        cur.append(e)
+        self._gen(cur, e, degNeeded - 1)
+        cur.pop()
+
+    def _getFeatureRelation(self):
+      for degree in range(self.degree + 1):
+        self._gen([], 0, degree)
+    
+    def transform(self, inputFeature):
+      newFeatures = []
+      for relation in self.relations:
+        val = 1
+        for featureIndex in relation:
+          val *= inputFeature[featureIndex]
+        newFeatures.append(val)
+      return newFeatures
+
+  def polyTransform(self):
+    featureCount = len(self.dataSet[0]) - 1
+    self.polyEngine = self.polyFeatureTransform(featureCount, degree= self.degree)
+    for sampleSet in self.dataSet:
+      features = sampleSet[:featureCount]
+      sampleSet[:featureCount] = self.polyEngine.transform(features)
+
+class basicLinearRegression(modelSetup): # gradient decsent approach
   def __init__(self, 
                inputDataSet, 
                initWeights= None, 
                epsilon= 0.000001, 
                learningRate= 0.001, 
-               parsingData= (None,None,None),  # [0]: comma/pip/semicolumn/space-separated value
+               parsingData= (None,None,None),   # [0]: comma/pip/semicolumn/space-separated value
                                                 # [1]: header included in file
                                                 # [2]: indexing column included
                featureScaling= True,
-               outComeScaling= True):
-    super().__init__(inputDataSet, initWeights, epsilon, learningRate, parsingData, featureScaling)
+               outComeScaling= True,
+               modelDegree= 1,
+               iterationLogTrigger= 1000):
+    super().__init__(inputDataSet, initWeights, epsilon, learningRate, parsingData, featureScaling, modelDegree, iterationLogTrigger)
     # scaling outPut/outCome also. But why? Because we are doing the arithmetic computation
     # and we have to deal with the overflow lol
     self.outComeScaling = False
@@ -83,8 +131,8 @@ class basicLinearRegression(modelSetup): # gradinet decsent approach
 
     return mean, stdDev
   
-  def _modelEvalutaion(self): # R-square evaluation
-    actualMean = sum([val[-1] for val in self.dataSet]) / self.sizeDataSet
+  def _modelEvaluaion(self): # R-square evaluation
+    actualMean = sum([val[-1] / self.sizeDataSet for val in self.dataSet])
     sst = 0 # sumOfSquareErrors: proportional to the variance of the actual data
     sse = 0 # sumOfSquareOfResidualErrors: proportional to the variance of the prodicted data
     for sampleIndex in range(self.sizeDataSet):
@@ -95,13 +143,29 @@ class basicLinearRegression(modelSetup): # gradinet decsent approach
     return sst, sse, goodnessOfFit
   
   def predict(self, inputData):
-    if len(inputData) == self.featureCount:
-      result = self.weights[0]
-      for i in range(self.featureCount):
-        if self.featureScaling:
-          mean, stdDev = self.featureScaling[i][0], self.featureScaling[i][1]
+    if len(inputData) == len(self.scalers[0]):
+    # input -> scaleBefore -> poly -> scalseAfter -> prediction
+    # ----------------------------------------------------------
+    # 1st scale / scaleBefore
+      for i in range(len(inputData)):
+        mean, stdDev = self.scalers[0][i]
+        if stdDev == 0:
+          inputData[i] = 0
+        else:
           inputData[i] = (inputData[i] - mean) / stdDev
-        result += self.weights[i + 1]*inputData[i]
+    # polynomial extention
+      inputData = self.polyEngine.transform(inputData)
+    # 2nd scale / scaleAfter
+      for i in range(1, len(inputData)):
+        mean, stdDev = self.scalers[1][i - 1]
+        if stdDev == 0:
+          inputData[i] = 0
+        else:
+          inputData[i] = (inputData[i] - mean) / stdDev      
+    # predict
+      result = 0
+      for i in range(self.featureCount):
+        result += self.weights[i]*inputData[i]
 
       if self.outComeScaling:
         result = result*self.outComeScaling[1] + self.outComeScaling[0]
@@ -112,8 +176,8 @@ class basicLinearRegression(modelSetup): # gradinet decsent approach
 
   def _predict(self, sampleIndex): # from the hypothesis function
     result = self.weights[0]
-    for i in range(self.featureCount):
-      result += self.weights[i + 1]*self.dataSet[sampleIndex][i]
+    for i in range(1, self.featureCount): # offSet by 1 for bias term
+      result += self.weights[i]*self.dataSet[sampleIndex][i]
 
     return result
   
@@ -127,11 +191,12 @@ class basicLinearRegression(modelSetup): # gradinet decsent approach
   def computeGradient(self, featureIndex):
     result = 0
     for sampleIndex in range(self.sizeDataSet):
-      result += (self.dataSet[sampleIndex][-1] - self._predict(sampleIndex))*(1 if featureIndex == 0 else self.dataSet[sampleIndex][featureIndex - 1])
+      result += (self.dataSet[sampleIndex][-1] - self._predict(sampleIndex))*self.dataSet[sampleIndex][featureIndex]
 
     return result*(self.learningRate / self.sizeDataSet)
 
   def fitModel(self, log = None):
+    print(f"-----TRAINING START-----\n")
     currentCost = self.computeMse()
     self.costHistory = [currentCost]
     if log:
@@ -140,10 +205,12 @@ class basicLinearRegression(modelSetup): # gradinet decsent approach
         f.write(f"[weights], current MSE, shifting in MSE \n")
         f.write(f"{self.weights}, {currentCost}, 0 \n")
         while True:
+          if self.iterationCount % self.logTrigger == 0:
+            print(f"||| {self.iterationCount}\t training iterations passed; model'current cost: {currentCost}")
           self.iterationCount += 1
           updWeights = self.weights.copy()
-          for weightIndex in range(self.featureCount + 1):
-            updWeights[weightIndex] += self.computeGradient(weightIndex)
+          for featureIndex in range(self.featureCount):
+            updWeights[featureIndex] += self.computeGradient(featureIndex)
 
           self.weights = updWeights
           updateCost = self.computeMse()
@@ -152,16 +219,18 @@ class basicLinearRegression(modelSetup): # gradinet decsent approach
           #
           f.write(f"{self.weights}, {updateCost}, {shifting} \n")
           #
-          if (shifting) < self.epsilon or self.iterationCount > 200000:
+          if (shifting) < self.epsilon or updateCost > currentCost:
             break
           else:
             currentCost = updateCost
     else:
       while True:
+        if self.iterationCount % self.logTrigger == 0:
+          print(f"||| {self.iterationCount}\t training iterations passed; model'current cost: {currentCost}")
         self.iterationCount += 1
         updatePar = self.weights.copy()
-        for weightIndex in range(self.featureCount + 1):
-          updatePar[weightIndex] += self.computeGradient(weightIndex)
+        for featureIndex in range(self.featureCount):
+          updatePar[featureIndex] += self.computeGradient(featureIndex)
 
         self.weights = updatePar
         updateCost = self.computeMse()
@@ -169,7 +238,7 @@ class basicLinearRegression(modelSetup): # gradinet decsent approach
         shifting = abs((currentCost - updateCost) / currentCost)
         #
         #
-        if (shifting) < self.epsilon or self.iterationCount > 200000:
+        if (shifting) < self.epsilon or updateCost > currentCost:
           break
         else:
           currentCost = updateCost
@@ -209,44 +278,74 @@ class basicLogisticRegression(modelSetup): # grandient ascent approach
                initWeights= None, 
                epsilon= 0.000001, 
                learningRate= 0.001, 
-               parsingData= (None,None,None),  # [0]: comma/pip/semicolumn/space-separated value
+               parsingData= (None,None,None),   # [0]: comma/pip/semicolumn/space-separated value
                                                 # [1]: header included in file
                                                 # [2]: indexing column included
-               featureScaling= True):
-    super().__init__(inputDataSet, initWeights, epsilon, learningRate, parsingData, featureScaling)
+               featureScaling= True,
+               modelDegree= 1,
+               logEventTrigger= 1000):
+    super().__init__(inputDataSet, initWeights, epsilon, learningRate, parsingData, featureScaling, modelDegree, logEventTrigger)
 
-  def predict(self, inputData):
-    result = self.weights[0]
-    for i in range(self.featureCount):
-      if self.featureScaling:
-        mean, stdDev = self.featureScaling[i]
-        inputData[i] = (inputData[i] - mean) / stdDev      
-      result += self.weights[i + 1]*inputData[i]
+  def predict(self, inputData, inputIsScaled= False):
+    def prd(data):
+      result = 0
+      for i in range(1, len(data)):
+        result += self.weights[i]*inputData[i]
+      return 1 / (1 + math.e**(-result))
 
-    return 1 / (1 + math.e**(-result))
+    if not inputIsScaled:
+      if len(inputData) == len(self.scalers[0]):
+      # input -> scaleBefore -> poly -> scalseAfter -> prediction
+      # ----------------------------------------------------------
+      # 1st scale / scaleBefore
+        for i in range(len(inputData)):
+          mean, stdDev = self.scalers[0][i]
+          if stdDev == 0: inputData[i] = 0
+          else: inputData[i] = (inputData[i] - mean) / stdDev
+      # polynomial extention
+        inputData = self.polyEngine.transform(inputData)
+      # 2nd scale / scaleAfter
+        for i in range(1, len(inputData)):
+          mean, stdDev = self.scalers[1][i - 1]
+          if stdDev == 0: inputData[i] = 0
+          else: inputData[i] = (inputData[i] - mean) / stdDev      
+      # predict
+        return prd(inputData)
+      else:
+        print(f"Input size does not match the model! {len(inputData)} != {len(self.scalers[0])} pls check again")
+        return None
+    else:
+      if len(inputData) == len(self.scalers[1]):
+        return prd(inputData)
+      else:
+        print(f"Input size does not match the model! {len(inputData)} != {len(self.scalers[1])} pls check again")
+        return None
 
   def _predict(self, sampleIndex):
-    result = self.weights[0]
+    result = 0
     for i in range(self.featureCount):
-      result += self.weights[i + 1]*self.dataSet[sampleIndex][i]
-    
+      result += self.weights[i]*self.dataSet[sampleIndex][i]
     return 1 / (1 + math.e**(-result))
   
   def computeLogLikelihood(self):
     llh = 0 # log Likelihood
     for sampleIndex in range(self.sizeDataSet):
-      llh += self.dataSet[sampleIndex][-1]*math.log(self._predict(sampleIndex)) + (1 - self.dataSet[sampleIndex][-1])*math.log(1 - self._predict(sampleIndex))
+      label = self.dataSet[sampleIndex][-1]
+      pred = self._predict(sampleIndex)
+      pred = min(1-(1e-15), pred)
+      llh += label*math.log(pred) + (1 - label)*math.log(1 - pred)
 
     return llh
   
   def computeGradient(self, freatureIndex):
     grd = 0
     for sampleIndex in range(self.sizeDataSet):
-      grd += (self.dataSet[sampleIndex][-1] - self._predict(sampleIndex))*(1 if freatureIndex == 0 else self.dataSet[sampleIndex][freatureIndex - 1])
+      grd += (self.dataSet[sampleIndex][-1] - self._predict(sampleIndex))*self.dataSet[sampleIndex][freatureIndex]
 
     return self.learningRate*grd
   
   def fitModel(self, log= None):
+    print(f"-----TRAINING START-----\n")
     curLlh = self.computeLogLikelihood()
     self.costHistory = [math.e**curLlh]
     if log:
@@ -254,9 +353,11 @@ class basicLogisticRegression(modelSetup): # grandient ascent approach
       with open('iterations.txt', mode = 'a') as f:
         f.write(f"""["Weights"], current log likelihood, shifting in Llh \n {self.weights}, {curLlh}, 0 \n""")
         while True:
+          if self.iterationCount % self.logTrigger == 0:
+            print(f"||| {self.iterationCount}\t training iterations passed; model'current log-likelihood: {curLlh}")
           updateWeights = self.weights.copy()
           self.iterationCount += 1
-          for freatureIndex in range(len(self.weights)):
+          for freatureIndex in range(self.featureCount):
             updateWeights[freatureIndex] += self.computeGradient(freatureIndex) 
 
           self.weights = updateWeights
@@ -264,22 +365,24 @@ class basicLogisticRegression(modelSetup): # grandient ascent approach
           self.costHistory.append(math.e**newLlh)
           change = abs((newLlh - curLlh) / curLlh)
           f.write(f"{self.weights}, {curLlh}, {change} \n")
-          if change < self.epsilon:
+          if change < self.epsilon or newLlh < curLlh:
             break
           else:
             curLlh = newLlh
     else:
       while True:
+        if self.iterationCount % self.logTrigger == 0:
+          print(f"||| {self.iterationCount}\t training iterations passed; model'current log-likelihood: {curLlh}")
         updateWeights = self.weights.copy()
         self.iterationCount += 1
-        for freatureIndex in range(len(self.weights)):
+        for freatureIndex in range(self.featureCount):
           updateWeights[freatureIndex] += self.computeGradient(freatureIndex) 
 
         self.weights = updateWeights
         newLlh = self.computeLogLikelihood()
         self.costHistory.append(math.e**newLlh)
         change = abs((newLlh - curLlh) / curLlh)
-        if change < self.epsilon:
+        if change < self.epsilon or newLlh < curLlh:
           break
         else:
           curLlh = newLlh
@@ -293,7 +396,7 @@ class basicLogisticRegression(modelSetup): # grandient ascent approach
     plt.show()
 
   def showTestTable(self):
-    predictedProbabilities = [self.predict(inputData) for inputData in self.testSet]
+    predictedProbabilities = [self.predict(inputData[:len(inputData) - 1], inputIsScaled= False) for inputData in self.testSet]
     predictedLabels = [(1 if val >= 0.5 else 0) for val in predictedProbabilities]
     tableData = []
     for i in range(len(self.testSet)):
